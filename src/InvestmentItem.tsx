c45dab5b01848300f39cfc34484622b5a674828a
shravya-gif <95952294+shravya-gif@users.noreply.github.com>
2023-11-13 22:42:02 +0530

    Designing

--- src/InvestmentItem.tsx
+++ src/InvestmentItem.tsx
@@ -1,8 +1,31 @@
 import { Investment } from "./UI";
 function InvestmentItem(props: { investment: Investment }) {
+  const {
+    Scrip,
+    Quantity,
+    Price,
+    Cost,
+    Amount,
+    Value,
+    PnL,
+    Return,
+  } = props.investment;
   return (
     <div>
-      <h1>{JSON.stringify(props.investment)}</h1>
+      <div className="flex flex-row justify-center items-center p-5">
+        <div className="bg-red-600 h-28 w-full">
+          {Scrip}
+          <div className="">{Price}</div>
+        </div>
+        <div className="bg-amber-400 h-28 w-full">Quantity {Quantity}
+        <div>Avg.Cost {Cost}</div>
+        <div>Inv.Amount {Amount}</div></div>
+        <div className="bg-lime-600 h-28 w-full">Market Value {Value}</div>
+        <div className="bg-cyan-600 h-28 w-full">Unrealized P&L{PnL}</div>
+        <div className="bg-blue-800 h-28 w-full"></div>
+        <div className="bg-pink-600 h-28 w-full"></div>
+      </div>
+      {/* <h1 className="">{JSON.stringify(props.investment)}</h1> */}
     </div>
   );
 }
--- src/InvestmentList.tsx
+++ src/InvestmentList.tsx
@@ -3,11 +3,13 @@ import InvestmentItem from "./InvestmentItem";
 // import "./InvestmentList.css";
 function InvestmentList(props: { investment: Investment[] }) {
   return (
-    <div>
+    <div className="flex flex-col">
+   
       <h2>Investment List</h2>
       {props.investment.map((item, index) => (
         <InvestmentItem key={index} investment={item} />
       ))}
+      
     </div>
   );
 }
