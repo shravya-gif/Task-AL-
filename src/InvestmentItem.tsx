aab28effc62d175b659838f58ad99fa7e30ef9ef
shravya-gif <95952294+shravya-gif@users.noreply.github.com>
2023-11-14 15:00:19 +0530

    responsiveness

--- src/InvestmentItem.tsx
+++ src/InvestmentItem.tsx
@@ -4,7 +4,7 @@ import { BsBox2Fill } from "react-icons/bs";
 import { AiFillCreditCard } from "react-icons/ai";
 import { BarIndicator } from "./BarIndicator";
 import MarketValue from "./MarketValue";
-import logoImage from './Images/IShares_by_BlackRock_Logo.png';
+import logoImage from "./Images/IShares_by_BlackRock_Logo.png";
 function InvestmentItem(props: { investment: Investment }) {
   const {
     Scrip,
@@ -19,7 +19,7 @@ function InvestmentItem(props: { investment: Investment }) {
 
   return (
     <div className="flex items-center gap-5 flex-col sm:flex-col lg:flex-row">
-      <GiHamburgerMenu size={24} color="#333"  />
+      <GiHamburgerMenu size={24} color="#333" />
 
       <div className="flex flex-col w-20 ">
         <h1>{Scrip}</h1>
@@ -30,23 +30,25 @@ function InvestmentItem(props: { investment: Investment }) {
       </div>
 
       {/* Image  */}
-      <div><img src={logoImage} alt="Logo" className="h-5 w-15"/></div>
+      <div>
+        <img src={logoImage} alt="Logo" className="h-5 w-15" />
+      </div>
 
       {/* Investment details */}
-      <div className="flex flex-col w-80">
-        <div className="flex gap-8">
+      <div className="flex flex-col w-80 justify-around">
+        <div className="flex gap-4">
           <BsBox2Fill size={20} color="#333"></BsBox2Fill>
           <h1>Quantity</h1>
           <div className="flex-1"></div>
           <h1>{Quantity}</h1>
         </div>
-        <div className="flex gap-8">
+        <div className="flex gap-0">
           <h1 className="h-10 w-10">@</h1>
           <h1>Avg Cost</h1>
           <div className="flex-1"></div>
           <h1>{`$${Cost}`}</h1>
         </div>
-        <div className="flex gap-8">
+        <div className="flex gap-4">
           <AiFillCreditCard size={24} />
           <h1>Invested Amt</h1>
           <div className="flex-1"></div>
@@ -118,16 +120,14 @@ function InvestmentItem(props: { investment: Investment }) {
         </div>
       </div>
       {/* Buy and sell */}
-      <div className="flex flex-col">
-        <button className="bg-white border-2 border-orange-500 text-black py-2 px-4 rounded transition duration-300 mb-2 hover:bg-orange-500 hover:text-white">
+      <div className="flex flex-col sm:flex-row md:flex-col lg:flex-col">
+        <button className="flex-1 bg-white border-2 border-orange-500 text-black py-2 px-4 rounded transition duration-300 mb-2 sm:mb-0 sm:mr-2 hover:bg-orange-500 hover:text-white">
           Buy
         </button>
-
-        <button className="bg-white border-2 border-orange-500 mb-3 text-black py-2 px-4 rounded transition duration-300 hover:bg-orange-500 hover:text-white">
+        <button className="flex-1 bg-white border-2 border-orange-500 text-black py-2 px-4 rounded transition duration-300 hover:bg-orange-500 hover:text-white">
           Sell
         </button>
       </div>
-     
     </div>
   );
 }
--- src/InvestmentList.tsx
+++ src/InvestmentList.tsx
@@ -5,17 +5,16 @@ import DonutChart from "./Chart";
 
 function InvestmentList(props: { investment: Investment[] }) {
   return (
-    <div className="flex flex-col sm:flex-row md:flex-col lg:flex-col">
+    <div className="flex flex-col sm:flex-row  md:flex-row lg:flex-row lg:items-center">
       {/* Investment List */}
-      <div className="flex flex-col">
-        <h2>Investment List</h2>
+      <div className="flex flex-col ">
         {props.investment.map((item, index) => (
           <InvestmentItem key={index} investment={item} />
         ))}
       </div>
 
       {/* DonutChart */}
-      <div className="h-10 w-10 sm:flex-col lg:order-last">
+      <div className="h-10 w-10 sm:flex-col md:order-last lg:order-last">
         <DonutChart />
       </div>
     </div>
